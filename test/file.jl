1d8d4e31bfb5e7a9dba9bfa5cbcbaf8a3664b3a5
CarlBittendorf <carl.bittendorf@gmail.com>
2021-06-19 20:02:13 +0200

    add tests

---
+++ test/file.jl
@@ -0,0 +1,62 @@
+using BVH
+using Test
+
+
+@testset "file" begin
+    g = load("Example.bvh") |>
+        global_positions! |>
+        remove_joint!(7) |>
+        remove_joint!(13) |>
+        remove_joints!("J_L_Bale", "J_R_Bale", "J_L4", "J_L3", "J_L1", "J_T12", "J_T10", "J_T9", "J_T8", "J_T6", "J_T5", "J_T4", "J_T3", "J_T2") |>
+        remove_joint!("J_T1", "J_C7") |>
+        remove_joint!("J_C7", "J_C6") |>
+        remove_joint!("J_C6", "J_C5") |>
+        remove_joints!("J_C4", "J_C3", "J_C2", "J_C1", "J_Atlas") |>
+        optimize_offsets!
+
+    @test nv(g) == 25
+    @test ne(g) == 24
+
+    dict = Dict(  "J_L_Hip" => "lThighBend", 
+                    "J_R_Hip" => "rThighBend", 
+                    "J_L_Knee" => "lShin", 
+                    "J_R_Knee" => "rShin", 
+                    "J_L_Ankle" => "lFoot", 
+                    "J_R_Ankle" => "rFoot", 
+                    "J_L5" => "abdomenLower", 
+                    "J_L2" => "abdomenUpper", 
+                    "J_T11" => "chestLower", 
+                    "J_T7" => "chestUpper", 
+                    "J_L_Clavicle" => "lCollar", 
+                    "J_R_Clavicle" => "rCollar", 
+                    "J_L_Shoulder" => "lShldrBend", 
+                    "J_R_Shoulder" => "rShldrBend", 
+                    "J_L_Elbow" => "lForearmBend", 
+                    "J_R_Elbow" => "rForearmBend", 
+                    "J_L_Hand" => "lHand", 
+                    "J_R_Hand" => "rHand", 
+                    "J_C5" => "neckLower")
+
+    g |>
+        add_joint!("J_L_Hip", "J_L_Knee", "lThighTwist") |>
+        add_joint!("J_R_Hip", "J_R_Knee", "rThighTwist") |>
+        add_joint!("J_L_Shoulder", "J_L_Elbow", "lShldrTwist") |>
+        add_joint!("J_R_Shoulder", "J_R_Elbow", "rShldrTwist") |>
+        add_joint!("J_L_Elbow", "J_L_Hand", "lForearmTwist") |>
+        add_joint!("J_R_Elbow", "J_R_Hand", "rForearmTwist") |>
+        rename!(dict)
+
+    @test nv(g) == 31
+    @test ne(g) == 30
+
+    name!(g, 1, "ROOT hip")
+    oabdomen = offset(g, 1, find(g, "abdomenLower"))
+    add_joint!(g, "hip", "pelvis", [0.0, 0.0, oabdomen[3]], [find(g, "lThighBend"), find(g, "rThighBend")])
+    ofoot = offset(g, find(g, "lShin"), find(g, "lFoot"))
+    add_joint!(g, "lFoot", "lMetatarsals", [0.0, ofoot[3] / 70, ofoot[3] / 70])
+    add_joint!(g, "rFoot", "rMetatarsals", [0.0, ofoot[3] / 70, ofoot[3] / 70])
+    exclude = [find(g, "lCollar"), find(g, "rCollar"), find(g, "lThighBend"), find(g, "rThighBend")]
+
+    @test nv(g) == 34
+    @test ne(g) == 33
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,81 +3,9 @@ using Test
 
 
 @testset "Graph" begin
+    include("graph.jl")
+end
 
-    g = BVHGraph(3)
-    @test add_edge!(g, 1, 2) == true
-    @inferred add_edge!(g, 2, 3, offset = [1.0, 3.0, 5.0])
-    @inferred add_vertex!(g, name = "JOINT L3", sequence = :ZXY)
-    
-    @inferred has_vertex(g, 2)
-    @test has_vertex(g, 2) == true
-    @test has_vertex(g, 5) == false
-
-    @inferred has_edge(g, 2, 3)
-    @test has_edge(g, 2, 3) == true
-    @test has_edge(g, 3, 2) == false
-
-    @inferred props(g, 3)
-    
-    @inferred AbstractString name(g)
-    @inferred AbstractString name(g, 3)
-    @inferred AbstractString name!(g, "Test.bvh")
-    @inferred AbstractString name!(g, 1, "JOINT hip")
-    @inferred AbstractString name!(g, 2, "JOINT L5")
-
-    @inferred sequence(g)
-    @inferred sequence(g, 3)
-    sequence!(g, :ZXY)
-    sequence!(g, 1, :ZXY)
-
-    @inferred nframes(g)
-    @test nframes(g) == 1
-    @inferred frames(g)
-
-    @inferred frametime(g)
-    @test frametime(g) == 0.01
-
-    @inferred Matrix{Float64} rotations(g, 1)
-    @inferred Matrix{Float64} rotations!(g, 1, rand(Float64, 10, 3))
-    @inferred Matrix{Float64} rotations(g, 1, 5)
-
-    @inferred Matrix{Float64} positions(g)
-    @inferred Matrix{Float64} positions(g, 1)
-    @inferred Matrix{Float64} positions!(g, rand(Float64, 10, 3))
-    @inferred Matrix{Float64} positions!(g, 1, rand(Float64, 10, 3))
-    @inferred Matrix{Float64} positions(g, 1, 5)
-    
-    @inferred Vector{Float64} offset(g)
-    @inferred Vector{Float64} offset(g, 1, 2)
-    @inferred Vector{Float64} offset!(g, rand(Float64, 3))
-    @inferred Vector{Float64} offset!(g, 1, 2, rand(Float64, 3))
-
-    @inferred find(g, "JOINT L5")
-    @test find(g, "JOINT hip") == 1
-    @test find(g, "JOINT L5") == 2
-    
-    @inferred eltype(g)
-    @inferred edgetype(g)
-    @test is_directed(g) == true
-    @inferred print(g)
-
-    @inferred nv(g)
-    @test nv(g) == 4
-    @inferred ne(g)
-    @test ne(g) == 2
-
-    @inferred vertices(g)
-    @inferred edges(g)
-
-    @inferred inneighbors(g, 3)
-    @test inneighbors(g, 3) == [2]
-    @inferred outneighbors(g, 1)
-    @test outneighbors(g, 1) == [2]
-
-    @inferred rem_edge!(g, 2, 3)
-    @inferred rem_vertex!(g, 3)
-    @inferred fadj(g)
-    @inferred badj(g)
-
-    @inferred BVHGraph(3)
+@testset "File" begin
+    include("file.jl")
 end
